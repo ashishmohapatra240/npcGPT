1a801ff0015a4dd26d75040ba5c57bbe4a4cdd24
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2023-07-03 01:29:02 +0530

    update: deploy to vercel3

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-routes.get("/", async function (req, res) {
-    res.send(`Reached home!`);
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", async function (req, res) {
+    res.send("Reached home!");
 });
